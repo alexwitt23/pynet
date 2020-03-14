trying to fix backprop bug

nautilus.py
@@ -89,8 +89,7 @@ linear_model = Model(
 loss_fn = NLLLoss()
 optimizer = sgd(linear_model, lr=1e-4, weight_decay=1e-3)
 
-for i in range(10000):
-
+for i in range(1):
     out = linear_model(np.array(X))
     if not isinstance(out, tuple):
         out = (out,)
@@ -105,7 +104,7 @@ for i in range(10000):
 scores = linear_model(X)
 predicted_class = np.argmax(scores, axis=1)
 print('training accuracy: %.2f' % (np.mean(predicted_class == y)))
-
+"""
 h = 0.02
 x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
@@ -123,5 +122,5 @@ plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral, alpha=0.8)
 plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
 plt.xlim(xx.min(), xx.max())
 plt.ylim(yy.min(), yy.max())
-plt.show()
-
+#plt.show()
+"""

pynet/layers/fullyconnected.py
@@ -20,10 +20,14 @@ class linear:
         """
         Given input size m, and output size n, create [m X n] matrix 
         of weights.
+
+        N: batch size.
+        M: Output size of hidden layer.
+        D: Input size.
         """
         self.input_size = input_size
         self.output_size = output_size
-        self.weights = np.random.rand(input_size, output_size) * 0.01
+        self.weights = np.random.randn(input_size, output_size) * 0.01
         self.input = np.empty([input_size])
 
         if activation is None:
@@ -41,33 +45,52 @@ class linear:
             self.biases = np.zeros((1, output_size))
 
     def __call__(self, x: np.ndarray) -> np.ndarray:
-        """Forward pass."""
+        """Forward pass.
+        
+        Args:
+            - x: Input Tensor.
+
+        Returns:
+            Apply an affine transformation y = Wx + b.
+        """
         self.input = x
         self.out = np.dot(self.input, self.weights)
+        # Apply bias if included
         if self.use_bias:
             self.out += self.biases
+
         return self.out
 
-    def backprop(self, x: np.ndarray, lr: float, decay: float) -> np.ndarray:
+    def backprop(self, dout: np.ndarray, lr: float, decay: float) -> np.ndarray:
         """
         Backprop of gradient to weights, biases, and chain rule.
-        See derivation: http://cs231n.stanford.edu/handouts/linear-backprop.pdf
+        See derivation: http://cs231n.stanford.edu/handouts/linear-backprop.pdf.
+
+        Args:
+            dout: The derivative of loss w.r.t this layer's output. (N, M)
+            lr: learning rate 
+            decay: the weight decay value to apply.
+        
+        Returns:
+            Derivative of loss w.r.t this layer's input.
         """
+
         if self.use_activation:
-            x = self.activation.backprop(x)
+            dout = self.activation.backprop(dout)
+
         # Backprop to input for this layer
-        dinput = np.dot(x, self.weights.transpose())
+        dinput = np.dot(dout, self.weights.transpose())
 
         # Adjust weights
         self.weights -= (
-            np.dot(self.input.transpose(), x) * lr 
+            np.dot(self.input.transpose(), dout) * lr 
         ) 
-
+        """
         if decay > 0:
             self.weights -= self.regularization.apply(self.weights, decay) * lr
 
         if self.use_bias:
-            self.biases -= (x.sum(axis=0) * lr)
-        
+            self.biases -= (dout.sum(axis=0) * lr)
+        """
         return dinput
 

pynet/layers/log_softmax.py
@@ -18,15 +18,16 @@ class LogSoftmax:
     
     def __call__(self, x: np.ndarray) -> np.ndarray:
         self.input = x
-        inter = np.exp(x) - np.amax(x, axis=self.axis, keepdims=True)
+        inter = np.exp(x) #- np.amax(x, axis=self.axis, keepdims=True)
         
         self.out = np.log(inter) - np.log(inter.sum(axis=self.axis, keepdims=True))
         self.softmax = np.exp(self.out)
         return self.out
 
     def backprop(self, x: np.ndarray, lr: float, weight_decay: float) -> np.ndarray:
-        batch_size = self.input.shape[0]
         # Apply gradient, which is 1 - p(x) where x = target.
         # Then complete chain rule with incoming gradient
-        self.softmax += np.multiply(1 - self.softmax, x)
-        return self.softmax / batch_size
+        print(self.softmax)
+        self.softmax += np.multiply(1, x)
+        print(self.softmax / self.input.shape[0])
+        return self.softmax / self.input.shape[0]