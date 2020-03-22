f7f2a3b990c24157c6a1431898d4963a8458f526
alex <awitt2399@utexas.edu>
2020-03-22 00:00:41 -0700

    DBSCAN working!

--- pynet/unsupervised_learning/dbscan.py
+++ pynet/unsupervised_learning/dbscan.py
@@ -13,7 +13,7 @@ from pynet.core import utils
 
 class DBSCAN:
 
-    def __init__(self, epsilon, min_pts) -> None:
+    def __init__(self, epsilon: float, min_pts: int) -> None:
         """
         Args:
             epsilon: The distance between points in a cluster. In this case
@@ -23,7 +23,7 @@ class DBSCAN:
         self.epsilon = epsilon
         self.min_pts = min_pts
         self.already_counted: List[int] = []
-        self.all_clusters: List[np.ndarray] = []
+        self.all_clusters: List[List[int]] = []
 
     def fit(self, x: np.ndarray) -> None:
         """Fit the data.
@@ -35,47 +35,57 @@ class DBSCAN:
         
         ??????
         """
-        assert len(x.shape) == 2, "Input must be shape "
+        assert len(x.shape) == 2, "Input must be 2 dimensional "
         self.x = x
         for i in range(self.x.shape[0]):
             # Already found neighbors here, move on
             if i in self.already_counted:
                 continue
+            neighbor_ids = self._find_neighbors(self.x[i])
 
-            neighbors = self._find_neighbors(self.x[i])
             # See if this counts as a cluster
-            if neighbors.shape[0] >= self.min_pts:
+            if len(neighbor_ids) >= self.min_pts:
                 # Go through the cluster and expand to all possible points. 
-                # Keep track of the points with known neighbors
-                self.already_counted.append(i)
                 # Call recursive function to find the entire cluster starting from 
                 # this list of neighbors
-                cluster = self._expand_cluster(x[neighbors])
-                self.all_clusters.append(cluster)
-        return None
+                self.all_clusters.append(
+                    self._expand_cluster(i, neighbor_ids)
+                )
+                
 
+        return self.all_clusters
 
     def _find_neighbors(self, source: np.ndarray) -> np.ndarray:
         """Loop through and get distance to this point."""
-        # Record the ids of points that are <= epsilon threshold
-        cluster_ids = np.array(
-            [idx2 for idx2, point in enumerate(self.x[:]) if utils.euclidean_distance(source, point) <= self.epsilon]
+        # Record the _ids_ of points that are <= epsilon threshold
+        return (
+            [idx for idx, point in enumerate(self.x[:]) if utils.euclidean_distance(source, point) <= self.epsilon]
         )
 
-        return cluster_ids
-
-    def _expand_cluster(self, points: np.ndarray) -> np.ndarray:
-        """Recursivly go through cluster to expand to all points within epsilon."""
-        for point in points[:]:
+    def _expand_cluster(
+        self, 
+        current_point: int, 
+        neighbor_ids: np.ndarray
+    ) -> np.ndarray:
+        """Recursively go through cluster to expand to all points within epsilon.
+        
+        Args:
+        ???????????/"""
+        cluster = [current_point]
+        for idx in neighbor_ids[:]:
             # Make sure this is a new point to consider
-            print(point, self.already_counted)
-            if point not in self.already_counted:
+            if idx not in self.already_counted:
+                self.already_counted.append(idx)
                 # Get this neighbors of this point
-                neighbors = self.already_counted.append(point)
-                if neighbors.shape[0] >= self.min_pts:
-                    additional_cluster = self._expand_cluster(neighbors)
+                neighbor_ids = self._find_neighbors(self.x[idx])
 
-        return additional_cluster
+                # Check that point is a core point
+                if len(neighbor_ids) >= self.min_pts:
+                    cluster.extend(self._expand_cluster(idx, neighbor_ids))
+                else:
+                    cluster.extend([idx])
+
+        return cluster
         
 
 if __name__ == '__main__':
@@ -87,5 +97,27 @@ if __name__ == '__main__':
     X, y_true = make_blobs(n_samples=300, centers=num_centers,
                         cluster_std=0.60, random_state=0)
 
-    dbscan = DBSCAN(epsilon=1, min_pts=3)
-    dbscan.fit(X)
+    dbscan = DBSCAN(epsilon=.3, min_pts=3)
+    clusters = dbscan.fit(X)
+    
+    plt.scatter(
+        X[:, 0], 
+        X[:, 1], 
+        c='blue', 
+        s=50, 
+        cmap='viridis'
+    )
+
+    # Create colors for each cluster
+    colors = np.random.rand(len(clusters), 3)
+    for idx, cluster in enumerate(clusters):
+        
+        plt.scatter(
+            X[cluster[:], 0], 
+            X[cluster[:], 1], 
+            c=colors[idx], 
+            s=50, 
+            cmap='viridis'
+        )
+        
+    plt.show()
