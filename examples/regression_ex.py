017740f4f50fd8018c9d032f9f713aa24cf59149
alexwitt2399 <awitt2399@utexas.edu>
2020-04-12 15:06:42 -0700

    added polynomial regression.

--- examples/regression_ex.py
+++ examples/regression_ex.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
 
     from sklearn.datasets.samples_generator import make_blobs
     import matplotlib.pyplot as plt
-
+    
+    """
     num_points = 100
     y = np.sort(np.random.uniform(-1, 1, (num_points)))
     x = np.linspace(-1, 1, num=num_points)
@@ -22,7 +23,18 @@ if __name__ == "__main__":
     a, b = linear_regression.fit(x, y)
 
     # Get the range to plot line on
-
     plt.scatter(x, y, c="blue", s=50, cmap="viridis")
     plt.plot(x, a * x + b)
+    plt.show()
+    """
+    np.random.seed(0)
+    x = np.sort(np.random.normal(-10, 10, (200)))
+    y = np.polynomial.polynomial.polyval(x, [6, 1, 1, 1])
+    plt.scatter(x, y, s=10)
+
+    mlr = regression.PolynomialRegression(x, y, degree=3)
+    beta = mlr.fit()
+
+    out = np.polynomial.polynomial.polyval(x, beta)
+    plt.plot(x, out)
     plt.show()
